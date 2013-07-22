47e4006fbba526826c7b728834ac7d2f3cba14a5
Alexander Dinauer <alexander@dinauer.at>
2013-07-22 21:40:04 +0200

    change gradle type definition to enable reuse (multiple uses of the gradle type are now possible)

--- manifests/jboss.pp
+++ manifests/jboss.pp
@@ -3,23 +3,38 @@ Exec {
 	path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ]
 }
 
-define gradle($task) {
+class gradle {
 	file { 'link gradle':
 		path   => '/usr/bin/gradle',
 		ensure => 'link',
 		target => '/opt/tools/gradle/gradlew'
 	}
+}
 
-	exec { 'gradle task':
-		command => "gradle $task",
+define gradle::task($task, $build_file_path = '') {
+	include gradle
+
+	if $build_file_path == '' {
+		$task_to_execute = $task
+	} else {
+		$task_to_execute = "-b $build_file_path $task"
+	}
+
+	exec { "gradle $task_to_execute":
+		command => "gradle $task_to_execute",
 		require => File['link gradle']
 	}
 }
 
-gradle { 'hello world':
-	task => '-b /opt/tools/gradle/build.gradle helloWorld'
+
+Gradle::Task {
+	build_file_path => '/opt/tools/gradle/build.gradle'
 }
 
-#gradle { 'tasks':
-#	task => '-b /opt/tools/gradle/build.gradle tasks'
-#}
+gradle::task { 'hello world':
+	task => 'helloWorld'
+}
+
+gradle::task { 'testet':
+	task => 'tasks'
+}

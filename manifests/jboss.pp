68300ff33a869b5449f73468fb6b3b7c05d09a30
Alexander Dinauer <alexander@dinauer.at>
2013-07-22 21:10:51 +0200

    define gradle type (not reusable yet)

--- manifests/jboss.pp
+++ manifests/jboss.pp
@@ -3,13 +3,23 @@ Exec {
 	path => [ '/bin/', '/sbin/', '/usr/bin/', '/usr/sbin/' ]
 }
 
-file { 'link gradle':
-	path   => '/usr/bin/gradle',
-	ensure => 'link',
-	target => '/opt/tools/gradle/gradlew'
+define gradle($task) {
+	file { 'link gradle':
+		path   => '/usr/bin/gradle',
+		ensure => 'link',
+		target => '/opt/tools/gradle/gradlew'
+	}
+
+	exec { 'gradle task':
+		command => "gradle $task",
+		require => File['link gradle']
+	}
 }
 
-exec { 'gradle hello world':
-	command => 'gradle -b /opt/tools/gradle/build.gradle helloWorld',
-	require => File['link gradle']
+gradle { 'hello world':
+	task => '-b /opt/tools/gradle/build.gradle helloWorld'
 }
+
+#gradle { 'tasks':
+#	task => '-b /opt/tools/gradle/build.gradle tasks'
+#}
